1f344d10e1ae9e15a144ec5cdf6076f685069191
javier <xavixastro@gmail.com>
2020-04-27 14:36:54 -0700

    Implement Users and Sessions controllers

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,10 +2,26 @@ class Api::SessionsController < ApplicationController
 
     #Login
     def create
+        @user = User.find_by_credentials(
+            params[:user][:email],
+            params[:user][:password]
+        )
+        if @user
+            login!(@user)
+            render 'api/users/show'
+        else
+            render json: ["Invalid credentials"], status: 401
+        end
     end
 
     #Logout
     def destroy
+        if logged_in?
+            logout!
+            render json: {}
+        else
+            render json: ["Invalid action"], status: 404
+        end 
     end
 
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,22 @@ class Api::UsersController < ApplicationController
 
     #Sign Up
     def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 404
+        end
     end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :first_name, :last_name,
+        :birthdate, :genre)
+    end
+    
 
 end
