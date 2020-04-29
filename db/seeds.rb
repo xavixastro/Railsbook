6784a41d9ce96f0ee05a9b4fb88631a0d0a41bcf
javier <xavixastro@gmail.com>
2020-04-29 10:42:35 -0700

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+users = User.create([{ email: 'javi@aa.io', password: '123456', first_name: 'Javier', last_name: 'Castro', birthdate: '2000-04-04', gender: 'male'},
+{ email: 'pamela@aa.io', password: '123456', first_name: 'Pamela', last_name: 'Castro', birthdate: '2010-06-06', gender: 'female'}])
+
+profiles = Profile.create([{owner_id: users[0].id}, {owner_id: users[1].id} ])
