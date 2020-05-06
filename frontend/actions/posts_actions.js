f62eba419205668af063a1c4babfebaf6947b4f7
javier <xavixastro@gmail.com>
2020-05-06 00:18:55 -0700

    Posts action creators

---
+++ frontend/actions/posts_actions.js
@@ -0,0 +1,43 @@
+import * as PostApiUtil from '../util/posts_api_util';
+
+export const RECEIVE_POST = 'RECEIVE_POST';
+export const RECEIVE_POST_DETAIL = 'RECEIVE_POST_DETAIL';
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
+
+
+const receivePost = (post) => ({
+    type: RECEIVE_POST,
+    post
+});
+
+const receivePosts = (posts) => {
+    return {
+        type: RECEIVE_POSTS,
+        posts
+    };
+};
+
+const receivePostDetail = (payload) => { 
+    return {
+        type: RECEIVE_POST_DETAIL,
+        payload
+    }
+};
+
+
+export const requestPosts = () => dispatch => {
+    return PostApiUtil.fetchPosts() 
+        .then(posts =>  dispatch(receivePosts(posts)))
+};
+
+export const requestPost = (postId) => dispatch => {
+    return PostApiUtil.fetchTea(postId)
+        .then(payload => {
+            dispatch(receivePostDetail(payload))
+        })
+};
+
+export const createPost = post => dispatch => {
+    return PostApiUtil.createPost(post)
+        .then(post => dispatch(receivePost(post)))
+};
