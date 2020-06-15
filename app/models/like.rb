637961c100e5a1ce1966cca2a15729a7574fdd3d
javier <xavixastro@gmail.com>
2020-06-15 10:49:12 -0700

    Update likes model

--- app/models/like.rb
+++ app/models/like.rb
@@ -9,4 +9,16 @@
 #  updated_at :datetime         not null
 #
 class Like < ApplicationRecord
+
+    validates :post_id, :user_id, presence: true
+    validates :post_id, uniqueness: { scope: :user_id }
+
+    belongs_to :user, 
+        foreign_key: :user_id, 
+        class_name: :User
+
+    belongs_to :post, 
+        foreign_key: :post_id, 
+        class_name: :Post
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -26,5 +26,10 @@ class Post < ApplicationRecord
         class_name: :Comment, 
         dependent: :destroy
 
+    has_many :likes, 
+        foreign_key: :post_id, 
+        class_name: :Like, 
+        dependent: :destroy
+
     
 end
